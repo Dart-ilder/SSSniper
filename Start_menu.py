d5b863b29ba43a30920d094135c919d1962a86e7
jacob <denisov.iar@phystech.edu>
2019-11-26 12:31:04 +0300

    menu

--- Start_menu.py
+++ Start_menu.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
+import sys
 
 def _from_rgb(rgb):
     """translates an rgb tuple of int to a tkinter friendly color code
@@ -24,7 +24,6 @@ canvas.pack()
 tk.update()
 
 
-
 class Menu():
     def __init__(self, canvas, status):
         self.canvas = canvas
@@ -45,6 +44,9 @@ class Menu():
         self.PREV_B = ('Stickmen/prev_b.png', 380, 330)
         self.BACK_B = ('Stickmen/back_b.png', 30, 60)
         self.BACK_S = ('Stickmen/back_s.png', 30, 60)
+        self.EXIT_S = ('Stickmen/exit_s.png', 600, 600)
+        self.EXIT_B = ('Stickmen/exit_b.png', 600, 600)
+
 
         self.NAMEload = ImageTk.PhotoImage(Image.open(self.NAMEPic[0]))
         self.CHOOSE_S_load = ImageTk.PhotoImage(Image.open(self.CHOOSE_S[0]))
@@ -62,17 +64,14 @@ class Menu():
         self.PREV_S_load = ImageTk.PhotoImage(Image.open(self.PREV_S[0]))
         self.BACK_B_load = ImageTk.PhotoImage(Image.open(self.BACK_B[0]))
         self.BACK_S_load = ImageTk.PhotoImage(Image.open(self.BACK_S[0]))
+        self.EXIT_S_load = ImageTk.PhotoImage(Image.open(self.EXIT_S[0]))
+        self.EXIT_B_load = ImageTk.PhotoImage(Image.open(self.EXIT_B[0]))
 
         self.NAME = canvas.create_image(self.NAMEPic[1], self.NAMEPic[2], image=self.NAMEload)
-        if status == 'menu':
-            self.START = canvas.create_image(self.START_S[1], self.START_S[2], image=self.START_S_load)
-            self.SETTINGS = canvas.create_image(self.SETTINGS_S[1], self.SETTINGS_S[2], image=self.SETTINGS_S_load)
-            self.ABOUT = canvas.create_image(self.ABOUT_S[1], self.ABOUT_S[2], image=self.ABOUT_S_load)
-        elif status == 'settings':
-            pass
-        elif status == 'start':
-            self.CHOOSE = canvas.create_image(self.CHOOSE_S[1], self.CHOOSE_S[2], image=self.CHOOSE_S_load)
-
+        self.START = canvas.create_image(self.START_S[1], self.START_S[2], image=self.START_S_load)
+        self.SETTINGS = canvas.create_image(self.SETTINGS_S[1], self.SETTINGS_S[2], image=self.SETTINGS_S_load)
+        self.ABOUT = canvas.create_image(self.ABOUT_S[1], self.ABOUT_S[2], image=self.ABOUT_S_load)
+        self.EXIT = canvas.create_image(self.EXIT_S[1], self.EXIT_S[2], image=self.EXIT_S_load)
         canvas.update()
 
     def inside(obj, obj_s_load, x, y, canvas):
@@ -101,6 +100,8 @@ class Menu():
                                        self.SETTINGS_S, self.SETTINGS_B)
         self.ABOUT = Menu.PICchange(self.ABOUT, self.ABOUT_S_load, x, y, canvas, self.ABOUT_B_load, self.ABOUT_S,
                                     self.ABOUT_B)
+        self.EXIT = Menu.PICchange(self.EXIT, self.EXIT_S_load, x, y, canvas, self.EXIT_B_load, self.EXIT_S,
+                                    self.EXIT_B)
 
     def motion_start(self, event):
         x, y = event.x, event.y
@@ -111,18 +112,17 @@ class Menu():
         self.PREV = Menu.PICchange(self.PREV, self.PREV_S_load, x, y, canvas, self.PREV_B_load, self.PREV_S,
                                    self.PREV_B)
         self.BACK = Menu.PICchange(self.BACK, self.BACK_S_load, x, y, canvas, self.BACK_B_load, self.BACK_S,
-                                  self.BACK_B)
+                                   self.BACK_B)
 
     def motion_settings(self, event):
         x, y = event.x, event.y
         self.BACK = Menu.PICchange(self.BACK, self.BACK_S_load, x, y, canvas, self.BACK_B_load, self.BACK_S,
                                    self.BACK_B)
 
-
     def go_to_menu(self, event):
         x, y = event.x, event.y
         if Menu.inside(self.START, self.START_S_load, x, y, self.canvas):
-            self.canvas.delete(self.SETTINGS, self.ABOUT, self.START)
+            self.canvas.delete(self.SETTINGS, self.ABOUT, self.START, self.EXIT)
             self.HOLE = canvas.create_image(self.HOLEPic[1], self.HOLEPic[2], image=self.HOLEload)
             self.CHOOSE = canvas.create_image(self.CHOOSE_S[1], self.CHOOSE_S[2], image=self.CHOOSE_S_load)
             self.NEXT = canvas.create_image(self.NEXT_S[1], self.NEXT_S[2], image=self.NEXT_S_load)
@@ -132,15 +132,20 @@ class Menu():
             tk.bind('<Button-1>', menu.go_back)
             tk.mainloop()
         elif Menu.inside(self.SETTINGS, self.SETTINGS_S_load, x, y, self.canvas):
-            self.canvas.delete(self.ABOUT, self.START, self.SETTINGS)
+            self.canvas.delete('all')
             self.BACK = canvas.create_image(self.BACK_S[1], self.BACK_S[2], image=self.BACK_S_load)
+            self.NAME = canvas.create_image(self.NAMEPic[1], self.NAMEPic[2], image=self.NAMEload)
             tk.bind('<Button-1>', menu.go_back)
             tk.bind('<Motion>', menu.motion_settings)
         elif Menu.inside(self.ABOUT, self.ABOUT_S_load, x, y, self.canvas):
-            self.canvas.delete(self.ABOUT, self.START, self.SETTINGS)
+            self.canvas.delete('all')
+            self.NAME = canvas.create_image(self.NAMEPic[1], self.NAMEPic[2], image=self.NAMEload)
             self.BACK = canvas.create_image(self.BACK_S[1], self.BACK_S[2], image=self.BACK_S_load)
             tk.bind('<Button-1>', menu.go_back)
             tk.bind('<Motion>', menu.motion_settings)
+        elif Menu.inside(self.EXIT, self.EXIT_S_load, x, y, self.canvas):
+            sys.exit()
+
 
     def go_back(self, event):
         x, y = event.x, event.y
@@ -150,6 +155,7 @@ class Menu():
             self.START = canvas.create_image(self.START_S[1], self.START_S[2], image=self.START_S_load)
             self.SETTINGS = canvas.create_image(self.SETTINGS_S[1], self.SETTINGS_S[2], image=self.SETTINGS_S_load)
             self.ABOUT = canvas.create_image(self.ABOUT_S[1], self.ABOUT_S[2], image=self.ABOUT_S_load)
+            self.EXIT = canvas.create_image(self.EXIT_S[1], self.EXIT_S[2], image=self.EXIT_S_load)
             tk.bind('<Button-1>', menu.go_to_menu)
             tk.bind('<Motion>', menu.motion_menu)
             tk.mainloop()
